d3eaf90190b6b67f37b9ab82d42ec1f9df5d854b
Henry Kane <118148408+zhqnb6666@users.noreply.github.com>
2023-12-19 00:12:10 +0800

    Update DisplayCounter.v

--- seven_segment/DisplayCounter.v
+++ seven_segment/DisplayCounter.v
@@ -3,22 +3,20 @@ module DisplayCounter (
     input rst,
     input [6:0] counter_value,
     output reg [6:0] seg,
-    output reg [2:0] digit_select
+    output reg [1:0] digit_select
 );
-  reg [3:0] digit_0, digit_1, digit_2;
+  reg [3:0] digit_0, digit_1;
   always @(posedge clk or posedge rst) begin
     if (rst) begin
       digit_0 <= 4'b0000;
       digit_1 <= 4'b0000;
-      digit_2 <= 4'b0000;
     end else begin
       digit_0 <= counter_value % 10;
       digit_1 <= (counter_value / 10) % 10;
-      digit_2 <= (counter_value / 100) % 10;
     end
   end
-  wire [6:0] seg_0_wire, seg_1_wire, seg_2_wire;
-  reg [6:0] seg_0, seg_1, seg_2;
+  wire [6:0] seg_0_wire, seg_1_wire;
+  reg [6:0] seg_0, seg_1;
 
   DigitToSegment digit0 (
       digit_0,
@@ -28,18 +26,13 @@ module DisplayCounter (
       digit_1,
       seg_1_wire
   );
-  DigitToSegment digit2 (
-      digit_2,
-      seg_2_wire
-  );
 
   always @(posedge clk) begin
     seg_0 <= seg_0_wire;
     seg_1 <= seg_1_wire;
-    seg_2 <= seg_2_wire;
   end
   reg [16:0] counter;
-  reg [ 1:0] counter_select;
+  reg counter_select;
   always @(posedge clk or posedge rst) begin
     if (rst) begin
       counter <= 0;
@@ -47,9 +40,6 @@ module DisplayCounter (
     end else if (counter == 10_000) begin
       counter <= 0;
       counter_select <= counter_select + 1;
-      if (counter_select == 2) begin
-        counter_select <= 0;
-      end
     end else begin
       counter <= counter + 1;
     end
@@ -57,21 +47,17 @@ module DisplayCounter (
 
   always @(counter_select) begin
     case (counter_select)
-      4'b000:  begin
+       1'b0:  begin
         seg <= seg_0;
-        digit_select <= 3'b001;
+        digit_select <= 3'b01;
         end
-        4'b001:  begin
+        1'b1:  begin
         seg <= seg_1;
-        digit_select <= 3'b010;
-        end
-        4'b010:  begin
-        seg <= seg_2;
-        digit_select <= 3'b100;
+        digit_select <= 3'b10;
         end
         default: begin
         seg <= 7'b0000000;
-        digit_select <= 3'b000;
+        digit_select <= 3'b00;
         end
     endcase
   end
