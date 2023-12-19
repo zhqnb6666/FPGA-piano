fd45f0e0a0b24353327618f4ba3d1115434acad9
Henry Kane <118148408+zhqnb6666@users.noreply.github.com>
2023-12-19 23:59:23 +0800

    Update and rename playsong.v to learningmod.v

--- playsong/learningmod.v
+++ playsong/learningmod.v
@@ -1,23 +1,18 @@
 module PlaySong(
     input clk,
     input rst,
+    input correct_key_press, // Input signal for correct key press
     output reg key_on,
-    output reg [3:0] key,
-    output [6:0] duration
+    output reg [3:0] key
 );
 
-assign duration = 7'd12;
-
-// Define the song notes and durations
-reg [3:0] song[0:23]; // Adjusted length for the song
+reg [3:0] song[0:23];
+reg [26:0] durations[0:23];
 integer i = 0;
-reg [25:0] counter = 0; // Duration counter
-parameter [3:0] song[0:23] = {
-    4'd0, 4'd0, 4'd4, 4'd4, 4'd5, 4'd5, 4'd4, 4'd3,
-    4'd3, 4'd2, 4'd2, 4'd1, 4'd1, 4'd0, 4'd4, 4'd4,
-    4'd3, 4'd3, 4'd2, 4'd2, 4'd1, 4'd1, 4'd0, 4'd0
-};
-/*initial begin
+reg [27:0] counter = 0;
+reg waiting_for_key_press = 1; // Flag to indicate waiting for the correct key press
+
+initial begin
     // "Twinkle, Twinkle, Little Star" notes (simplified)
     // Each pair of notes is the same, representing one syllable
     song[0] = 5'd0;  // C
@@ -44,28 +39,43 @@ parameter [3:0] song[0:23] = {
     song[21] = 5'd1; // D
     song[22] = 5'd0; // C
     song[23] = 5'd0; // C
-end*/
+end
+
+integer j;
+initial begin
+    // Fill the durations array with 10000000
+    for (j = 0; j < 24; j = j + 1) begin
+        durations[j] = 100000000;
+    end
+end
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         i <= 0;
         counter <= 0;
         key_on <= 0;
+        waiting_for_key_press <= 1;
     end
-    else if(counter == 26'd25000000) begin // Fixed duration for each note
+    else if (waiting_for_key_press && correct_key_press) begin
+        // Move to next note when correct key is pressed
+        waiting_for_key_press <= 0;
+    end
+    else if (counter >= durations[i]+50000000 ) begin
         key_on <= 0;
-        counter <= counter + 1 ;
-    end
-    else if (counter == 26'd50000000) begin // Fixed duration for each note
-        key <= song[i];
-        key_on <= 1;
         counter <= 0;
-        i <= (i + 1) % 24; // Loop back to the start of the song
+        key <= song[(i + 1) % 24];
+        i <= (i + 1) % 24;
+        waiting_for_key_press <= 1; // Wait for correct key press
+    end
+    else if (!waiting_for_key_press) begin
+        counter <= counter + 1;
+        if (counter == 50000000) begin
+            key_on <= 1;
+        end 
     end
     else begin
-        counter <= counter + 1;
+        // Do nothing
     end
 end
 
 endmodule
- 
