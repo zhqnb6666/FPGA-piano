a5ba17986fa432d209665db7d41a38578a8e8015
Henry Kane <118148408+zhqnb6666@users.noreply.github.com>
2023-12-24 18:55:06 +0800

    Update and rename learningmod.v to learningmode.v

--- fpga_piano1.0/learningmode.v
+++ fpga_piano1.0/learningmode.v
@@ -52,42 +52,4 @@ module LearningMode (
   end
 endmodule
 
-//   // Always block for LED control and note progression
-//   always @(posedge clk or posedge rst) begin
-//     if (rst) begin
-//       note_index <= 0;
-//       note_to_play <= 0;
-//       play_note <= 0;
-//       counter <= 0;
-//       note_buzzed <= 0;
-//     end else
-//     // Check if the correct key is pressed and the duration has elapsed
-//     if (key_pressed && (pressed_key == current_note) && (counter >= current_duration)) begin
-//       // Move to the next note
-//       note_index <= note_index + 1;
-//       counter <= 0;  // Reset the counter
-//       note_buzzed <= 0;  // Reset the note buzzed flag
-//     end else if(counter < current_duration) begin
-//       // Update the note to play
-//       note_to_play <= current_note;
-//       // Update the play note flag
-//       play_note <= 1;
-//       // Increment the counter
-//       counter <= counter + 1;
-//     end else begin
-//       note_buzzed <= 0;
-//     end
-
-//   end
-
-//   // Always block for note buzzing
-//   always @(posedge clk or posedge rst) begin
-//     if (rst) begin
-//       note_buzzed <= 0;
-//     end else if (!note_buzzed && counter < current_duration) begin
-//       // Buzz the note once when its duration starts
-//       note_buzzed <= 1;
-
-//     end
-//   end
 
